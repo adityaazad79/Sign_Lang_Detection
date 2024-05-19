8f9fce188034d063e3f743745046892da8151ef3
Aditya <adityaazad79@gmail.com>
2024-05-19 23:13:11 +0530

    first commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'SignLanguageDetection',
+    version= '1.0.0',
+    author= 'Aditya Azad',
+    author_email= 'adityaazad79@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
